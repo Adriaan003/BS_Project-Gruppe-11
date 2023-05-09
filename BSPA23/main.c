a8cb5b579a78eb7000782c50817b663307e90a02
rippeddeadlift <130327074+rippeddeadlift@users.noreply.github.com>
2023-05-09 21:19:01 +0200

    Add files via upload

--- BSPA23/main.c
+++ BSPA23/main.c
@@ -1,146 +1,229 @@
 #include <stdio.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#define SERVER_PORT 5678
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include "socket.h"
+#include "keyValStore.h"
+#include "semaphore.h"
 
 
-void hello(int clientSocket) {
-    char *msg = "Hello, World!\n";
-    send(clientSocket, msg, strlen(msg), 0);
-}
-
+typedef struct Data {
+    char key[10];
+    char value[50];
+} Data;
+Data newData[50];
 
 int main() {
-    int servSocket, clientSocket[3], client_len, status;
-    struct sockaddr_in server, client;
+    int clientSocket[3], client_len, shm_id, shm_id2, sem_id;
+    struct sockaddr_in client;
     pid_t pid[3];
 
-    int value;
 
-    // Festlegung eines neues Sockets, der:
-    // - über IPv4-Adressen kommuniziert
-    // - für die Verwendung von TCP konfiguriert ist
-    // - dem Betriebsystem die Wahl des Protokolltyps überlässt
+    int servSocket = setServSocket();
 
-    servSocket = socket(AF_INET, SOCK_STREAM, 0);
+    sem_id = createSemaphore();
 
-    // Diese Zeilen initialisieren den Server, um auf Verbindungen von Clients zu warten.
-    // 1. Adressfamilie = AF_INET (IPv4 basierte Kommunikation)
-    // 2. Server IP-Adresse = INADDR_ANY (erlaubt alle IP-Adressen des Rechners für das Socket)
-    // 3. Festlegung des Ports, auf dem der Server auf eingehende Verbindungen warten wird
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(SERVER_PORT);
+    // Implementierung gemeinsamen Speicherplatzes
+    // Shared Struct Data
 
-    //  HIER:
-    //  Sock - Unser File-descriptor, der an die Adresse gebunden werden soll
-    //  &server - Die Adresse im Netzwerk, an die das Socket gebunden werden soll
-    //  sizeof(server) - Die größe des zweiten Parameters (Struktur server)
-    if (0 > bind(servSocket, (struct sockaddr *) &server, sizeof(server))) {
-        perror("Fehlerfall: \n");
-        exit(EXIT_FAILURE);
 
+    Data *sharedData;
+
+    shm_id = shmget(IPC_PRIVATE, sizeof(Data) * 50, IPC_CREAT | 0644);
+
+    sharedData = (struct Data *) shmat(shm_id, NULL, 0);
+    for (int i = 0; i < 50; ++i) {
+        sharedData[i] = newData[i];
     }
 
 
-    // Wartemodus:
-    // Sock - unser Server Socket, auf dem die Verbindung stattfinden soll
-    // 3 - Anzahl der Clients in der Warteschlange
+    char *sharedArg;
+    shm_id2 = shmget(IPC_PRIVATE, sizeof(char) * 6, IPC_CREAT | 0644);
 
-    if (0 > listen(servSocket, 3)) {
-        perror("socket konnte nicht listen gesetzt werden\n");
-        exit(EXIT_FAILURE);
+
+    sharedArg = (char *) shmat(shm_id2, 0, 0);
+
+
+    if (shm_id == -1 || shm_id2 == -1) {
+        perror("Das Segment konnte nicht angelegt werden!");
+        exit(1);
     }
 
-    printf("Server wartet auf Port %d...\n", SERVER_PORT);
 
-    client_len = sizeof(client);
-
-    for (int i = 0; i < 3; ++i) { //Erzeugung Kindprozesse
-        pid[i] = fork();
+    for (int i = 0; i < 2; ++i) {
+        pid[i] = fork();        // Erzeugung Kind-Prozesse
         if (pid[i] == -1) {
             perror("Kind konnte nicht erzeugt werden!");
             exit(1);
         }
-        if (pid[i] == 0) {// Verhinderung des exponentiellen Wachstums der Kindprozesse
+        if (pid[i] == 0) {      // Verhinderung des exponentiellen Wachstums der Kind-Prozesse
             break;
         }
 
     }
 
 
-    for (int k = 0; k < 3; ++k) {
-        if (pid[k] == 0) { // Hier startet der Kindprozess. Multiclientfähig, da 3 Prozesse bereits erzeugt wurden
-            for (int j = 0; j < 3; ++j) {
-                if (clientSocket[j] != 0) {
-                    //  Falls eine Verbindung entsteht, wird diese mit der Methode accept() akzeptiert
-                    //  Das (leere) Objekt Client wird mit Information des Clients gefüllt (IP-Adresse & Port)
-                    //  Unser Rückgabewert gibt uns einen NEUEN fD, den wir für die Verbindung mit DIESEM Client nutzen können
-                    clientSocket[j] = accept(servSocket, (struct sockaddr *) &client, &client_len);
-
-                    char quit = ' ';
-                    while (quit != 'y') {
-                        char *buffer = malloc(100 * sizeof(char)); // Dynamische Platzfreihaltung (Array[100])
-
-                        sprintf(buffer, "Meine PID: %i\n", getpid());
-                        // Die Nachricht im Buffer wird an Client Socket verschickt
-                        send(clientSocket[j], buffer, strlen(buffer), 0);
+    for (int j = 0; j < 2; ++j) {
+        if (pid[j] == 0) {      // der Kind-Prozess startet hier seine Arbeit
 
 
-                        sprintf(buffer, "Bitte 1 fuer hallo oder 2 fuer exit eingeben\n");
-                        send(clientSocket[j], buffer, strlen(buffer), 0);
+            client_len = sizeof(client);
+            //  Falls eine Verbindung entsteht, wird diese mit der Methode accept() akzeptiert
+            //  Das (leere) Objekt Client wird mit Information des Clients gefüllt (IP-Adresse & Port)
+            //  Unser Rückgabewert gibt uns einen NEUEN fD, den wir für die Verbindung mit DIESEM Client nutzen können
+            clientSocket[j] = accept(servSocket, (struct sockaddr *) &client, &client_len);
 
 
-                        //Empfang der Daten vom Client. Die Daten werden im buffer gespeichert
-                        if (read(clientSocket[j], buffer, sizeof(int)) < 0) {
-                            perror("Es konnten keine Daten empfangt werden.");
-                            exit(EXIT_FAILURE);
-                        }
 
-                        //Fun "strtol" Konvertiert String into Long
-                        value = strtol(buffer, NULL, 0);
+            // Dynamische Platzreservierung  (Array[100])
+            char *nachricht = malloc(100);
 
-                        //value = ntohl(value);
+            sprintf(nachricht, "\nMeine PID: %i\n", getpid());
+            // Die Nachricht im Buffer wird an Client Socket verschickt
+            send(clientSocket[j], nachricht, strlen(nachricht), 0);
+            free(nachricht);
 
-                        switch (value) {
-                            case 1:
-                                hello(clientSocket[j]);
-                                break;
-                            case 2:
-                                sprintf(buffer, "y für Exit\n");
-                                send(clientSocket[j], buffer, strlen(buffer), 0);
-                                // scanf(" %c", &exit);
-                                read(clientSocket[j], &quit, sizeof(quit));
-                                break;
-                            default:
-                                sprintf(buffer, "Falsche Eingabe! 1 für Text, 2 für exit\n");
-                                send(clientSocket[j], buffer, strlen(buffer), 0);
-                                break;
-                        }
-                        free(buffer);
-                    }
-                    exit(0);
+
+            while (1) {
+
+                char eeeeeeee[20] = "Ich komme hier an\n";
+                char *buffer = malloc(BUFSIZ);
+
+
+                char arg1[50], arg2[50], arg3[100];
+                // Empfang der Daten vom Client. Die Daten werden im buffer gespeichert
+                if (read(clientSocket[j], buffer, BUFSIZ) < 0) {
+                    perror("Es konnten keine Daten empfangt werden.\t");
+                    exit(1);
                 }
+
+
+                sscanf(buffer, "%s %s %[^\n]", arg1, arg2, arg3);
+
+                char beg[3] = "BEG";
+                char end[3] = "END";
+
+                if (strncmp(arg1, beg, strlen("BEG")) == 0) {
+
+                    strcpy(sharedArg, beg);
+                }
+                char set[3] = "SET";
+                char get[3] = "GET";
+                char del[3] = "DEL";
+                char quit[4] = "QUIT";
+
+                // die BEG/END exklusive Transaktionen beginnen hier.
+
+                if (strncmp(arg1, beg, strlen("BEG")) == 0) {
+                    openSemaphore(sem_id);
+
+                    while (1) {
+                        if (read(clientSocket[j], buffer, BUFSIZ) < 0) {
+                            perror("Es konnten keine Daten empfangt werden.\t");
+                            exit(1);
+                        }
+                        sscanf(buffer, "%s %s %[^\n]", arg1, arg2, arg3);
+
+                        if (strncmp(arg1, set, strlen("SET")) == 0) {
+
+                            setKey(arg2, arg3, sharedData);
+
+
+                        } else if (strncmp(arg1, get, strlen("GET")) == 0) {
+                            *buffer = 0;
+
+                            buffer = getKey(arg2, sharedData);
+                            send(clientSocket[j], buffer, strlen(buffer), 0);
+
+
+                        } else if (strncmp(arg1, del, strlen("DEL")) == 0) {
+
+                            delKey(arg2, clientSocket[j], sharedData);
+
+
+                        } else if (strncmp(arg1, quit, strlen("QUIT")) == 0) {
+
+                            //buffer = "Client Socket wird geschloßen.\n";
+                            send(clientSocket[j], buffer, strlen(buffer), 0);
+                            close(clientSocket[j]);
+
+
+                        } else if (strncmp(arg1, end, strlen("END")) == 0) {
+                            strcpy(sharedArg, end);
+                            send(clientSocket[j], eeeeeeee, strlen(eeeeeeee), 0);
+                            closeSemaphore(sem_id);
+                            break;
+                        }
+                    }
+
+
+
+                    // closeSemaphore(sem_id);
+                }
+
+
+                //Standard Bedingungen, falls BEG/END nicht gewählt wurden
+
+                if (strncmp(sharedArg, beg, strlen("BEG")) != 0) {
+
+                    if (strncmp(arg1, set, strlen("SET")) == 0) {
+                        setKey(arg2, arg3, sharedData);
+
+                    } else if (strncmp(arg1, get, strlen("GET")) == 0) {
+                        *buffer = 0;
+
+                        buffer = getKey(arg2, sharedData);
+                        send(clientSocket[j], buffer, strlen(buffer), 0);
+
+
+                    } else if (strncmp(arg1, del, strlen("DEL")) == 0) {
+
+                        delKey(arg2, clientSocket[j], sharedData);
+
+
+                    } else if (strncmp(arg1, quit, strlen("QUIT")) == 0) {
+
+                        //buffer = "Client Socket wird geschloßen.\n";
+                        send(clientSocket[j], buffer, strlen(buffer), 0);
+                        close(clientSocket[j]);
+
+                    } else {
+                        continue;
+                    }
+                }
+                // memset(&arg1, 0, sizeof(arg1));
+                // closeSemaphore(sem_id);
             }
         }
     }
 
-// Hier chillt der Vater
-    for (int i = 0; i < 3; ++i) {
-        waitpid(pid[i], NULL, 0);
-        close(clientSocket[i]);
+
+
+// Vaterprozess wartet auf Kind-Prozesse
+    if (pid[0] != 0) {
+        for (int i = 0; i < 2; ++i) {
+            printf("Vater mit ID %i wartet auf weitere/n Kind-Prozesse.\n", getpid());
+            waitpid(pid[i], NULL, 0);
+        }
+        printf("Vater geht.");
+        close(servSocket);
     }
 
 
-    close(servSocket);
-
+    shmdt(sharedData);
+    shmctl(shm_id,
+           IPC_RMID, 0);
+    shmdt(sharedArg);
+    shmctl(shm_id2,
+           IPC_RMID, 0);
+    semctl(sem_id,
+           0, IPC_RMID);
 
     return 0;
-
 
 }
